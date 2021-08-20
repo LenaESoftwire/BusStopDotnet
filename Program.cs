7a7d3e7fa8ce12ead666ff326c0e6721410f10b9
Lena E <elena.evdokimova@softwire.com>
2021-08-20 16:00:24 +0100

    Returns 5 buses from exact stop
    Need to work on choosing one

--- Program.cs
+++ Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using BusStopDotnet.Responses;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BusStopDotnet
 {
     class Program
     {
+        public static bool ready = false;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var buses = ApiService.GetBusesForBusStop("490008660N").Result;
+            while (!ready) ;
+
+            ApiService.PrintNexFiveBuses(buses);
+        }
+
+        class ApiService
+        {
+            public static async Task<List<BusStopResponse>> GetBusesForBusStop(string busStop)
+            {
+                var client = new RestClient("https://api.tfl.gov.uk/");
+                var request = new RestRequest($"StopPoint/{busStop}/Arrivals", DataFormat.Json);
+
+                var response = await client.GetAsync<List<BusStopResponse>>(request);
+                ready = true;
+                Console.WriteLine(response.Count);
+
+                return response;
+            }
+
+            public static void PrintNexFiveBuses(List<BusStopResponse> buses)
+            {
+                buses.Sort(delegate (BusStopResponse x, BusStopResponse y)
+                {
+                    return x.TimeToStation.CompareTo(y.TimeToStation);
+                });
+
+                Console.WriteLine($"Busstop {buses[0].NaptanId} {buses[0].StationName}");
+                for (var i = 0; (i < buses.Count && i < 5); i++)
+                {
+                    var bus = buses[i];
+                    Console.Write($"Bus number {bus.LineId} ");
+                    Console.Write($"Going to {bus.DestinationName} ");
+                    Console.WriteLine($"Comes to platform {bus.PlatformName} in {bus.TimeToStation / 60} min");
+                }
+            }
         }
     }
 }
---
+++ Responses/BusStopResponse.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusStopDotnet.Responses
+{
+    class BusStopResponse
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("vehicleId")]
+        public string VehicleId { get; set; }
+
+        [JsonProperty("naptanId")]
+        public string NaptanId { get; set; }
+
+        [JsonProperty("stationName")]
+        public string StationName { get; set; }
+
+        [JsonProperty("lineId")]
+        public string LineId { get; set; }
+
+        [JsonProperty("lineName")]
+        public string LineName { get; set; }
+        
+        [JsonProperty("platformName")]
+        public string PlatformName { get; set; }
+
+        [JsonProperty("bearing")]
+        public int Bearing { get; set; }
+        
+        [JsonProperty("destinationNaptanId")]
+        public string DestinationStopID { get; set; }
+
+        [JsonProperty("destinationName")]
+        public string DestinationName { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime UtcTime { get; set; }
+        
+        [JsonProperty("timeToStation")]
+        public int TimeToStation { get; set; }
+        
+        [JsonProperty("currentLocation")]
+        public string CurrentLocation { get; set; }
+
+        public string Towards { get; set; }
+
+        [JsonProperty("expectedArrival")]
+        public DateTime ExpectedAtUtc { get; set; }
+
+        public string ModeName { get; set; }
+
+    }
+
+
+    //"$type": "Tfl.Api.Presentation.Entities.Prediction, Tfl.Api.Presentation.Entities",
+    //"id": "1717940254",
+    //"operationType": 1,
+    //"vehicleId": "LA19KAE",
+    //"naptanId": "490008660N",
+    //"stationName": "Lady Somerset Road",
+    //"lineId": "214",
+    //"lineName": "214",
+    //"platformName": "GY",
+    //"direction": "inbound",
+    //"bearing": "316",
+    //"destinationNaptanId": "",
+    //"destinationName": "Highgate Village",
+    //"timestamp": "2021-08-18T20:42:00.0577943Z",
+    //"timeToStation": 886,
+    //"currentLocation": "",
+    //"towards": "Highgate Village or Parliament Hill Fields",
+    //"expectedArrival": "2021-08-18T20:56:46Z",
+    //"timeToLive": "2021-08-18T20:57:16Z",
+    //"modeName": "bus",
+    //"timing": {
+    //  "$type": "Tfl.Api.Presentation.Entities.PredictionTiming, Tfl.Api.Presentation.Entities",
+    //  "countdownServerAdjustment": "00:00:24.9324166",
+    //  "source": "2021-08-18T19:50:42.361Z",
+    //  "insert": "2021-08-18T20:42:18.84Z",
+    //  "read": "2021-08-18T20:42:43.798Z",
+    //  "sent": "2021-08-18T20:42:00Z",
+    //  "received": "0001-01-01T00:00:00Z"
+    //}
+}
