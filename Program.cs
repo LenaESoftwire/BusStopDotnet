a27cf66b189a47d807ad0846c4126fbbba382aef
Lena E <elena.evdokimova@softwire.com>
2021-08-21 23:00:23 +0100

    Get busstops by coordinates

--- Program.cs
+++ Program.cs
@@ -9,15 +9,19 @@ namespace BusStopDotnet
     {
         static void Main(string[] args)
         {
-            Helper.GetCoordinatesByPostcode();
-            
-            var stopId = Helper.GetBusStopNaptanId();
-            var buses = Helper.GetBusesForBusStop(stopId);
-            //while (!ready) ;
+            var coordinates = Helper.GetCoordinatesByPostcode();
+
+            var stops = Helper.GetNaptanIds(coordinates.Latitude, coordinates.Longitude);
+
+            var stopNaptan = stops.StopPoints[0].NaptanId;
+            Console.WriteLine($"Stop {stopNaptan}");
+
+            //var stopId = Helper.GetBusStopNaptanId();
+            var buses = Helper.GetBusesForBusStop(stopNaptan);
 
             if (buses.Count == 1)
             {
-                Console.WriteLine($"Couln't find any buses coming from {stopId} bus stop");
+                Console.WriteLine($"Couln't find any buses coming from {stopNaptan} bus stop");
             }
 
             else
@@ -33,7 +37,7 @@ namespace BusStopDotnet
                 var client = new RestClient("https://api.tfl.gov.uk/");
                 var request = new RestRequest($"StopPoint/{busStop}/Arrivals", DataFormat.Json);
                 var response = client.Get<List<BusStopResponse>>(request);
-                Console.WriteLine(response.Data.Count);
+                Console.WriteLine($"There are {response.Data.Count} stops around");
 
                 return response.Data;
             }
@@ -53,6 +57,18 @@ namespace BusStopDotnet
                 return response.Result;
             }
 
+            public static BusStopListResponse GetNaptanIds(decimal latitude, decimal longitude)
+            {
+                var client = new RestClient("https://api.tfl.gov.uk/");
+                var request = new RestRequest($"StopPoint/?lat={latitude}&lon={longitude}&stopTypes=NaptanPublicBusCoachTram&radius=1000", DataFormat.Json);
+                var response = client.Get<BusStopListResponse>(request).Data;
+                Console.WriteLine(response.StopPoints.Count);
+
+                return response;
+
+
+            }
+
             public static void PrintNexFiveBuses(List<BusStopResponse> buses)
             {
                 buses.Sort(delegate (BusStopResponse x, BusStopResponse y)
@@ -60,14 +76,17 @@ namespace BusStopDotnet
                     return x.TimeToStation.CompareTo(y.TimeToStation);
                 });
 
-                Console.WriteLine($"Busstop {buses[0].NaptanId} {buses[0].StationName}");
-                Console.WriteLine("Here are the next 5 arriving buses");
-                for (var i = 0; (i < buses.Count && i < 5); i++)
+                if (buses.Count > 0)
                 {
-                    var bus = buses[i];
-                    Console.Write($"Bus number {bus.LineId} ");
-                    Console.Write($"going to {bus.DestinationName} ");
-                    Console.WriteLine($"arrives to platform {bus.PlatformName} in {bus.TimeToStation / 60} min");
+                    Console.WriteLine($"Busstop {buses[0].NaptanId} {buses[0].StationName}");
+                    Console.WriteLine("Here are the next 5 arriving buses");
+                    for (var i = 0; (i < buses.Count && i < 5); i++)
+                    {
+                        var bus = buses[i];
+                        Console.Write($"Bus number {bus.LineId} ");
+                        Console.Write($"going to {bus.DestinationName} ");
+                        Console.WriteLine($"arrives to platform {bus.PlatformName} in {bus.TimeToStation / 60} min");
+                    }
                 }
             }
 
---
+++ Responses/BusStopListResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusStopDotnet.Responses
+{
+    class BusStopNaptan
+    {
+        [JsonProperty("stationNaptan")]
+        public string NaptanId { get; set; }
+    }
+
+    class BusStopListResponse
+    {
+        [JsonProperty("stopPoints")]
+        public List<BusStopNaptan> StopPoints { get; set; }
+    }
+
+}
